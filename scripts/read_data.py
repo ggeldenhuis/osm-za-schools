cfa5acdda0529d6de573a19f1859b2476b338fb5
Gerhardus Geldenhuis <gerhardus.geldenhuis@gmail.com>
2013-03-20 22:25:17 +0000

    Adding basic parsing and some notes on data

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -7,31 +7,28 @@ import pprint
 # data/Masterlist_2012_Q4_Ordinary_Schools.csv
 
 def printdata():
-#  with open('data/Masterlist_2012_Q4_Ordinary_Schools.csv', 'rb') as csvfile:
-  with open('../data/short.example.data', 'rb') as csvfile:
-    datareader = csv.reader(csvfile, delimiter=',',quotechar='"')
-#    print datareader.fieldnames()
-#    print type(datareader)
- 
-#    pp = pprint.PrettyPrinter(indent=2)
-#    pp.pprint(datareader)
-    for row in datareader:
-      for item in row:
-        print item
-#      print type(row)
-#      print row
-#      pp.pprint(row)
+  csv_file = csv.DictReader(open('../data/short.example.data', 'rb'), delimiter=',', quotechar='"')
+  for line in csv_file:
+    row_being_build = ''
+    row_being_build += line['NatEmis']+','
+    row_being_build += line['EmisNo']+','
+    row_being_build += line['Institution']+','# fix name 
+    row_being_build += line['PaypointNo']+','
+    row_being_build += line['ExamNo']+','
+    row_being_build += line['ExamCentre']+','# make boolean
+    row_being_build += line['GIS_Long']+','
+    row_being_build += line['GIS_Lat']+','
+    row_being_build += line['NoFeeSchool']+','
+    print 'empty',line['Snap_Learners_2007']
+    # some years have empty data.
+    # average should only for years with data
+
+#    average_students = int(line['Snap_Learners_2007']) + int(line['Snap_Learners_2008'])
+#    print average_students/2
+#    print row_being_build   
 
 def main():
   printdata()
 
 if __name__ == '__main__':
   main()
-
-
-#>>> with open('eggs.csv', 'rb') as csvfile:
-#...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#...     for row in spamreader:
-#...         print ', '.join(row)
-#Spam, Spam, Spam, Spam, Spam, Baked Beans
-#Spam, Lovely Spam, Wonderful Spam
